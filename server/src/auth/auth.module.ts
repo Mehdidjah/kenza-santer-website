9acb1d2ca55c0ed90eea756d9164117f4c988317
Mehdidjah <phoenixytbdjah7@gmail.com>
2026-05-10 19:04:52 +0100

    Fix auth guard provider scope

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,11 +3,17 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AdminGuard } from './admin.guard';
+import { jwtExpiresIn, jwtSecret } from './jwt-options';
 
 @Module({
-  imports: [JwtModule],
+  imports: [
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: jwtExpiresIn },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, AdminGuard],
-  exports: [AdminGuard],
+  exports: [AdminGuard, JwtModule],
 })
 export class AuthModule {}
